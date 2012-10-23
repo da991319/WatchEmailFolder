137c23c234a8bc3737181b62a8464aff89a4b441
da991319 <castaingregis@yahoo.fr>
2012-10-23 16:31:39 -0600

    clean merge error

--- EmailInBox/Views/MainWindow.xaml.cs
+++ EmailInBox/Views/MainWindow.xaml.cs
@@ -48,26 +48,5 @@ namespace EmailInBox.Views
         {
             Process.Start(lastMessagePath);
         }
-
-        [MessageRecipient(Tag = "New Message")]
-        public void ShowBallon(MessageModel message)
-        {
-            lastMessagePath = message.Path;
-
-            var sb = new StringBuilder();
-
-            sb.AppendFormat("From: {0}{1}", message.From, Environment.NewLine);
-            sb.AppendFormat("To: {0}{1}", message.To, Environment.NewLine);
-            sb.AppendFormat("Subject: {0}{1}", message.Subject, Environment.NewLine);
-            sb.AppendFormat("Click here to read the email");
-
-            TaskIcon.ShowBalloonTip("Email Received", sb.ToString(), BalloonIcon.Info);
-        }
-
-        private void TaskIcon_TrayBalloonTipClicked(object sender, System.Windows.RoutedEventArgs e)
-        {
-            Process.Start(lastMessagePath);
-        }
-
     }
 }
