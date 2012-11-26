4adc8b9c8a9b5f69eb90e5ba233b786b3c00b761
Regis <castaingregis@yahoo.fr>
2012-11-26 16:00:23 -0700

    check if list is empty

--- EmailInBox/Utils/UpdateMessagesListTask.cs
+++ EmailInBox/Utils/UpdateMessagesListTask.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -15,23 +16,33 @@ namespace EmailInBox.Utils
     {
         public List<MessageModel> UpdateMessageList(List<MessageModel> initialList, string folderPath, int numberOfFile)
         {
-            var referenceDate = initialList.Max(m => m.DateReceived);
+            DateTime referenceDate = DateTime.Now;
+
+            if (initialList.Count > 0)
+                referenceDate = initialList.Max(m => m.DateReceived);
 
             var tempList = FilesRetrieve.RetrieveEmails(folderPath, numberOfFile);
 
-            var finalList = initialList.Where(m => tempList.Select(x => x.Path).Contains(m.Path)).ToList();
+            List<MessageModel> finalList;
+            if (initialList.Count > 0)
+            {
+                finalList = initialList.Where(m => tempList.Select(x => x.Path).Contains(m.Path)).ToList();
 
-            finalList.AddRange(tempList.Where(m => !initialList.Select(x => x.Path).Contains(m.Path))
-                .Select(x => new MessageModel
-                                 {
-                                     DateReceived = x.DateReceived,
-                                     From = x.From,
-                                     NewEmail = x.DateReceived > referenceDate,
-                                     Path = x.Path,
-                                     Subject = x.Subject,
-                                     To = x.To
-                                 }));
-            
+                finalList.AddRange(tempList.Where(m => !initialList.Select(x => x.Path).Contains(m.Path))
+                                           .Select(x => new MessageModel
+                                                            {
+                                                                DateReceived = x.DateReceived,
+                                                                From = x.From,
+                                                                NewEmail = x.DateReceived > referenceDate,
+                                                                Path = x.Path,
+                                                                Subject = x.Subject,
+                                                                To = x.To
+                                                            }));
+            }
+            else
+            {
+                finalList = tempList;
+            }
             return finalList.OrderByDescending(m => m.DateReceived).ToList();
         }
     }
