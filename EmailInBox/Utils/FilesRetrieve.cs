0fc12e1207ef2b08da06571f7b78438af89749b1
Regis <castaingregis@yahoo.fr>
2013-04-24 15:42:26 -0600

    handle when folder to watch doesn't exist

--- EmailInBox/Utils/FilesRetrieve.cs
+++ EmailInBox/Utils/FilesRetrieve.cs
@@ -12,20 +12,18 @@ namespace EmailInBox.Utils
     {
         public static List<MessageModel> RetrieveEmails(string path, int numberOfFile)
         {
-            var files = Directory.GetFiles(path, "*.eml");
-
-            return (from file in files
-                                let message = new MimeReader().GetEmail(file)
-                                select new MessageModel
-                                           {
-                                               DateReceived = message.DeliveryDate,
-                                               From = message.From.Address,
-                                               To = message.To.ToString(),
-                                               Path = file,
-                                               Subject = message.Subject
-                                           }).OrderByDescending(f => f.DateReceived).Take(numberOfFile).ToList();
-
-
+            return Directory.Exists(path)
+                       ? (from file in Directory.GetFiles(path, "*.eml")
+                          let message = new MimeReader().GetEmail(file)
+                          select new MessageModel
+                                     {
+                                         DateReceived = message.DeliveryDate,
+                                         From = message.From.Address,
+                                         To = message.To.ToString(),
+                                         Path = file,
+                                         Subject = message.Subject
+                                     }).OrderByDescending(f => f.DateReceived).Take(numberOfFile).ToList()
+                       : new List<MessageModel>();
         }
 
         public static MessageModel RetreiveEmail(string path)
